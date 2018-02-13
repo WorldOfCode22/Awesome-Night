ebe19c423e186b360bf130c71a9a83c5577f5e14
Sloan <gwaltneys2@winthrop.edu>
2018-02-13 16:35:31 -0500

    added database URI support feature

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DB_URI).then(
+  () => { console.log('Database Connected')},
+  err => { console.log("Database Error: " + err)})
 app.listen(process.env.PORT,()=>{
   console.log('App runing on port '+ process.env.PORT);
 })
