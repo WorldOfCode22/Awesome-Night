f0c5b8a0ac135aa3d54f52023ed067e7e3f7053b
Sloan <gwaltneys2@winthrop.edu>
2018-02-16 23:25:43 -0500

    website functional beta

--- routes/api.js
+++ routes/api.js
@@ -11,31 +11,7 @@ router.get('/bars/:location',(req,res)=>{
       location:req.params.location,
       radius: 40000,
     }).then(response=>{
-      for(let i =0; i<response.jsonBody.businesses.length;i++){
-        for(let x = 0; x<response.jsonBody.businesses[i].categories.length;x++){
-          if(response.jsonBody.businesses[i].categories[x].alias === 'bars'){
-            barResults.push(response.jsonBody.businesses[i]);
-            continue;
-          }
-        }
-      }
-      if(barResults.length === 20){
-      res.json({businesses: barResults});
-    }else{
-      for(let a = 0; a<response.jsonBody.businesses.length;a++){
-        for(let b = 0; b<response.jsonBody.businesses[a].categories.length;b++){
-          if(response.jsonBody.businesses[a].categories[b].alias !== 'bars'){
-            barResults.push(response.jsonBody.businesses[a]);
-            if(barResults.length === 20){
-              console.log(barResults.length);
-              res.json({businesses: barResults});
-              return;
-            }
-          }
-      }
-    }
-    res.json({businesses: barResults});
-    }
+      res.json({businesses:response.jsonBody.businesses});
   })
 })
 
